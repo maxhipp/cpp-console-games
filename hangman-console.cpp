edd767c0d8e70122156492a699288b3c5dadf925
Max Hipp <34799688+maxhipp@users.noreply.github.com>
2021-05-21 11:27:04 +0200

    main structure

--- hangman-console.cpp
+++ hangman-console.cpp
@@ -1,14 +1,49 @@
+/* Hangman by Max Hipp in 2021
+*  Have fun!
+*/
 #include <stdio.h>
 
 struct game{
+    int mode = 0;
+    bool running = false;
     char word[20];
+    int wordsize = 0;
     char tips[26];
-    int round;
+    int round = 0;
     char pName[10];
 }game;
 
+void requestMode(){
+    printf("Please choose a mode: (0)Classic (1)Computer: ");
+    scanf("%i", &game.mode);
+    //printf("\nWort: %s :", game.word);
+    printf("Please choose a word (max. 20 characters): ");
+    scanf("%s", &game.word);
+    //printf("%s", game.word);
+}
+
+bool initializeGame(){
+    requestMode();
+    for(int i = 0; i < sizeof(game.word); i++){
+        if(game.word[i] != NULL){
+            game.wordsize++;
+            printf(" _ ");
+        }
+    }
+    printf(" %i\n", game.wordsize);
+    game.running = true;
+}
+
+void rungame(){
+
+}
+
 int main(){
-    printf("Loading...");
-    printf("Loaded!");
+    printf("Loading...\n");
+    printf("Loaded!\n");
+    initializeGame();
+    while(game.running){
+
+    }
     return 0;
 }
